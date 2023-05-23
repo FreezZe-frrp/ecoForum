74b8e4fc24b90a69ba55672635d089ced76e768f
Fábio <fabiopinto.epbjc@gmail.com>
2023-05-23 11:09:36 +0100

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,11 @@ import SideBar from "../Components/SideBar";
 import Footer from "../Components/Footer";
 
 const Home = () => {
-  let cona = "foda se";
-
   return (
     <div>
       <Navbar />
       <SideBar />
       <Footer />
-      <p> {cona} </p>
     </div>
   );
 };
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,39 @@
 import Navbar from "../Components/Navbar";
 import { initializeApp } from "firebase/app";
+import "../styles/register.css";
 
 const Register = () => {
   return (
     <div>
       <Navbar />
-      Register
+      <form>
+        <label htmlFor="user">Username</label>
+        <input
+          type="text"
+          id="user"
+          name="user"
+          placeholder="Enter Username"
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          required
+        />
+        <label htmlFor="pass">Password</label>
+        <input
+          type="password"
+          id="pass"
+          name="pass"
+          placeholder="Enter Password"
+          required
+        />
+
+        <input type="submit" id="regis" name="regis" value="Register" />
+      </form>
     </div>
   );
 };
